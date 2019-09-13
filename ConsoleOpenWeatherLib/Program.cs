59f7183f42e2d15bba39ef0bb31574235b5a1c57
Nplus <skrause@fzi.de>
2019-09-13 07:30:11 +0200

    Time difference now uses a constant value

--- ConsoleOpenWeatherLib/Program.cs
+++ ConsoleOpenWeatherLib/Program.cs
@@ -10,15 +10,18 @@ namespace ConsoleOpenWeatherLib
         static async Task Main(string[] args)
         {
             var openWeatherService = new OpenWeatherService("c9f817379c6fed669c718214bae83ab8");
-            var query = new OpenWeatherQuery(new List<string>() {
+            var query = new OpenWeatherQuery(
+                new List<string>()
+                {
                     "Karlsruhe,de",
-                    "Stuttgart,de" },
-                    new List<OpenWeatherTimeSpecification>()
-                    {
-                        new OpenWeatherTimeSpecification(RelativeDay.NextDay, new TimeSpan(7,30,0), DateTime.Today),
-                        new OpenWeatherTimeSpecification(RelativeDay.NextDay, new TimeSpan(16,30,0), DateTime.Today)
-                    }
-                );
+                    "Stuttgart,de"
+                },
+                new List<OpenWeatherTimeSpecification>()
+                {
+                    new OpenWeatherTimeSpecification(RelativeDay.NextDay, new TimeSpan(7,30,0), DateTime.Today),
+                    new OpenWeatherTimeSpecification(RelativeDay.NextDay, new TimeSpan(16,30,0), DateTime.Today)
+                }
+            );
             var matches = await openWeatherService.GetForecasts(query);
 
             foreach (var match in matches)
